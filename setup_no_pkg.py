cbbf57ac7bce0a0719117d0114728505c222f0e5
huxiaoou <516984451@qq.com>
2025-07-29 13:56:58 +0800

    Update setup_no_pkg.py

--- setup_no_pkg.py
+++ setup_no_pkg.py
@@ -3,11 +3,17 @@ from Cython.Build import cythonize
 
 ext_modules = cythonize(
     [
-        "src/package_huxo/alg.py",
-        "src/package_huxo/alg2.py",
-        "src/package_huxo/alg3.py",
-        "src/package_sxzq/utl.py",
-    ]
+        # "src/package_huxo/alg.py",
+        # "src/package_huxo/alg2.py",
+        # "src/package_huxo/alg3.py",
+        # "src/package_sxzq/utl.py",
+        "src/package_huxo/*",
+        "src/package_sxzq/*",
+    ],
+    compiler_directives={
+        "language_level": "3",
+        "annotation_typing": False,
+    },
 )
 
 setup(
